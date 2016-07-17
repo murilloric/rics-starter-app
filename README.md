84cc39377c3a510c38cc2ef780a6c23ab7acbadd
murilloric <murilloric@gmail.com>
2016-07-17 00:29:09 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# rics-starer-app
+
+* Ionic -V 1.2.0
+* Angular -V 1.4.3 
+* Angular-cookies -V 1.5.0
+* Ui-router -V 0.2.13
+
+
+--- Run gulp to min app files and create dist folder------
---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var del = require('del');
+var processhtml = require('gulp-processhtml');
+
+gulp.task('concat-css', function() {
+  return gulp.src(['./app/dep/ionic-v1.2.0/css/ionic.min.css', './app/css/app.css'])
+    .pipe(concat('app.min.css'))
+    .pipe(gulp.dest('./dist/css/'));
+});
+
+gulp.task('concat-js', function() {
+  return gulp.src(['./app/dep/ionic-v1.2.0/js/ionic.bundle.min.js', './app/dep/ionic-v1.2.0/js/angular/angular-cookies.min.js', './app/js/app.js', './app/js/services.js', './app/js/controller.js', './app/js/filters.js'])
+    .pipe(concat('app.min.js'))
+    .pipe(gulp.dest('./app/concat/'));
+});
+
+gulp.task('min-js', ['concat-js'], function() {
+	return gulp.src('./app/concat/app.min.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js/'));
+});
+
+gulp.task('del-js', ['concat-js', 'min-js' , 'html-replace'], function() {
+	return del([
+    	'./app/concat',
+  	]);
+});
+
+var opts = {}
+gulp.task('html-replace', function () {
+    return gulp.src('./app/*.html')
+               .pipe(processhtml(opts))
+               .pipe(gulp.dest('dist'));
+});
+
+var filesToMove = [
+        './app/partials/**/*.*',
+        './app/fonts/**'
+    ];
+
+gulp.task('move',[], function(){
+  gulp.src(filesToMove, { base: './app' })
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean-dist',[], function(){
+	return del([
+    	'./dist/',
+  	]);
+});
+
+gulp.task('default', ['concat-css', 'concat-js', 'min-js', 'del-js' , 'html-replace', 'move']);
